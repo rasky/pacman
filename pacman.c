f5da5e45b6cf9e72fbea4a31adf7a67f13fcec80
Giovanni Bajo <rasky@develer.com>
2014-04-04 01:31:20 +0200

    Add main loop

--- pacman.c
+++ pacman.c
@@ -7,6 +7,11 @@ int main(int argc, char *argv[])
 
     while (hw_poll())
     {
+        uint8_t *screen;
+        int pitch;
+
+        hw_beginframe(&screen, &pitch);
+        hw_endframe();
     }
 
     return 0;
