762a6092827d08b65c87c84f6a05e310da5b4c10
Giovanni Bajo <rasky@develer.com>
2014-04-11 17:16:27 +0200

    Sound support in hw.c

--- hw.c
+++ hw.c
@@ -1,13 +1,23 @@
 #include "hw.h"
 #include <SDL.h>
 #include <SDL_framerate.h>
+#include <assert.h>
+#include <time.h>
+
+#define HW_AUDIO_NUMBUFFERS 8
 
 static SDL_Surface *screen;
 static SDL_Surface *frame;
 static FPSmanager fps;
+static int16_t AUDIO_BUF[HW_AUDIO_NUMBUFFERS][HW_AUDIO_NUMSAMPLES];
+static int audio_buf_index_w=2, audio_buf_index_r=0;
 uint8_t *keystate;
+static clock_t frameclock;
+static int framecounter;
 
 #define SPLIT 20
+
+static void fill_audio(void *userdata, uint8_t* stream, int len);
 
 void hw_init(void)
 {
@@ -18,7 +28,7 @@ void hw_init(void)
     }
     atexit(SDL_Quit);
 
-    screen=SDL_SetVideoMode(288+224+SPLIT, 288, 32,SDL_DOUBLEBUF);
+    screen=SDL_SetVideoMode(288+224+SPLIT, 288, 32, SDL_DOUBLEBUF);
     if (screen == NULL)
     {
        printf("Unable to set video mode: %s\n", SDL_GetError());
@@ -27,7 +37,23 @@ void hw_init(void)
 
     frame = SDL_CreateRGBSurface(SDL_SWSURFACE, 36*8, 28*8, 32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0x0);
     SDL_initFramerate(&fps);
-    SDL_setFramerate(&fps, 60);
+    SDL_setFramerate(&fps, 100);
+
+    /* Initialize audio */
+    SDL_AudioSpec wanted;
+
+    wanted.freq = HW_AUDIO_FREQ;
+    wanted.format = AUDIO_S16;
+    wanted.channels = 1;
+    wanted.samples = HW_AUDIO_NUMSAMPLES;
+    wanted.callback = fill_audio;
+    wanted.userdata = NULL;
+    if (SDL_OpenAudio(&wanted, NULL) < 0) {
+        fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    SDL_PauseAudio(0);
 }
 
 int hw_poll(void)
@@ -62,7 +88,6 @@ void hw_beginframe(uint8_t **screen, int *pitch)
 
 void hw_endframe(void)
 {
-
     SDL_LockSurface(screen);
 
     uint8_t *spixels = screen->pixels;
@@ -104,7 +129,48 @@ void hw_endframe(void)
     SDL_UnlockSurface(frame);
     SDL_UnlockSurface(screen);
 
-    SDL_Flip(screen);
-    SDL_framerateDelay(&fps);
+    if (!frameclock || SDL_GetTicks() < frameclock)
+    {
+        SDL_Flip(screen);
+        //SDL_framerateDelay(&fps);
+    }
+
+    if (framecounter == 0)
+        frameclock = SDL_GetTicks();
+    frameclock += 1000/60+1;
+    framecounter += 1;
 }
 
+void hw_beginaudio(int16_t **buf)
+{
+    extern int framecounter;
+#if 0
+    if (audio_buf_index_w >= audio_buf_index_r + HW_AUDIO_NUMBUFFERS)
+        printf("[AUDIO](FC=%04d/R=%04d/W%04d) Warning: overflow audio buffer (producing too fast)\n", framecounter, audio_buf_index_r, audio_buf_index_w);
+#endif
+    *buf = AUDIO_BUF[audio_buf_index_w % HW_AUDIO_NUMBUFFERS];
+}
+
+void hw_endaudio(void)
+{
+    audio_buf_index_w += 1;
+}
+
+void fill_audio(void *userdata, uint8_t *stream, int len)
+{
+    static int audiocounter = 0;
+
+    if (audio_buf_index_r == audio_buf_index_w)
+    {
+        #if 0
+        extern int framecounter;
+        printf("[AUDIO](FC=%04d/AC=%04d/W=%04d) Warning: no audio generated, silencing...\n", framecounter, audiocounter, audio_buf_index_w);
+        #endif
+        memset(stream, 0, len);
+        return;
+    }
+
+    assert(sizeof(AUDIO_BUF) / HW_AUDIO_NUMBUFFERS == len);
+    memcpy(stream, AUDIO_BUF[audio_buf_index_r++ % HW_AUDIO_NUMBUFFERS], len);
+    ++audiocounter;
+}
--- hw.h
+++ hw.h
@@ -11,5 +11,13 @@ int hw_poll(void);
 void hw_beginframe(uint8_t **screen, int *pitch);
 void hw_endframe();
 
+
+#define HW_AUDIO_FREQ         44100
+#define HW_AUDIO_NUMSAMPLES   (HW_AUDIO_FREQ / 60)
+
+void hw_beginaudio(int16_t **buf);
+void hw_endaudio(void);
+
+
 #endif
 
